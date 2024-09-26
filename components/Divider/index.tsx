e8c6d106807d3e7ef13b3ffdb4f52d219da7f9fc
Douglas Trofino <douglas.trofino@outlook.com>
2024-09-26 10:19:43 -0300

    feat: create divider component

---
+++ components/Divider/index.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, View } from "react-native";
+
+export function Divider() {
+
+  return (
+    <View style={styles.base} />
+  );
+}
+
+const styles = StyleSheet.create({
+  base: {
+    width: '100%',
+    height: 1,
+    backgroundColor: 'gray',
+  }
+})

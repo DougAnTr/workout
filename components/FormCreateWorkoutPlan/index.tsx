932ce4895d629c4824de0050b4646c4ae08c6a66
Douglas Trofino <douglas.trofino@outlook.com>
2024-09-26 10:20:44 -0300

    chore: update FormCreateWorkout plan

--- components/FormCreateWorkoutPlan/index.tsx
+++ components/FormCreateWorkoutPlan/index.tsx
@@ -1,6 +1,22 @@
-import { Text } from "react-native";
+import { KeyboardAvoidingView, Text } from "react-native";
+import { Input } from "../Input";
+import { Divider } from "../Divider";
+import { Button } from "../Button";
 
 export function FormCreateWorkoutPlan() {
 
-  return <Text>Create your workout plan</Text>;
+  return (
+    <KeyboardAvoidingView>
+      <Text>Create your workout plan</Text>
+
+      <Input placeholder={'Plan name'} />
+      <Input placeholder={'Description'} />
+
+      <Divider />
+
+      <Text>Workouts</Text>
+
+      <Button>Add workout</Button>
+    </KeyboardAvoidingView>
+  );
 }
--- components/__tests__/FormCreateWorkoutPlan.test.tsx
+++ components/__tests__/FormCreateWorkoutPlan.test.tsx
@@ -2,6 +2,15 @@ import { render } from '../../utils/test-utils';
 import { FormCreateWorkoutPlan } from '../FormCreateWorkoutPlan';
 
 describe('<FormCreateWorkoutPlan/>', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.runOnlyPendingTimers();
+    jest.useRealTimers();
+  });
+
   it('should render correctly', () => {
     const snapshot = render(<FormCreateWorkoutPlan />).toJSON();
 
